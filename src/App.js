9e886b990f6dee10bdf534649687970209fae251
Mac User <sabbey37@gmail.com>
2017-09-06 14:20:06 -0400

    Adds navlinks and routes to app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {BrowserRouter, Route, Switch, Link} from 'react-router-dom';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <BrowserRouter>
+            <div>
+                <ul>
+                    <li><Link to="/Worldclocks">World Clocks</Link></li>
+                    <li><Link to="/Realculator">React Calculator</Link></li>
+                    <li><Link to="/Martah">Martah Train Tracker</Link></li>
+                </ul>
+                <Switch>
+                    <Route exact path = "/Worldclocks" component={} />
+                    <Route path = "/Realculator" component={} />
+                    <Route path = "/Martah" component={} />
+                    <Route render={() => (
+                        <p>Page not found: No Soup for you!</p>
+                        )} />
+                </Switch>
+            </div>
+        </BrowserRouter>
       </div>
     );
   }
