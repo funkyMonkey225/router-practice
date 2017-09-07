79337ade422e0ce69e423f3f0d9b08b276b8daeb
Mac User <sabbey37@gmail.com>
2017-09-07 13:40:18 -0400

    Updates styling

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Calculator from "./Calculator.js";
 import Marta from "./Marta.js";
 import Clock from "./Clock.js";
+import Home from "./Home.js";
 
 class App extends Component {
   render() {
@@ -16,13 +17,14 @@ class App extends Component {
                     <ul className="nav">
                         <li><NavLink to="/Worldclocks" activeStyle={{color: '#F9CF00'}}>World Clocks</NavLink></li>
                         <li><NavLink to="/Realculator" activeStyle={{color: '#F9CF00'}}>Realculator</NavLink></li>
-                        <li><NavLink to="/Martah" activeStyle={{color: '#F9CF00'}}>Martah Train Tracker</NavLink></li>
+                        <li><NavLink to="/MARTA" activeStyle={{color: '#F9CF00'}}>MARTA Train Tracker</NavLink></li>
                     </ul>
                 </div>
                 <Switch>
-                    <Route exact path = "/Worldclocks" component={Clock} />
+                    <Route exact path = "/" component={Home} />
+                    <Route path = "/Worldclocks" component={Clock} />
                     <Route path = "/Realculator" component={Calculator} />
-                    <Route path = "/Martah" component={Marta} />
+                    <Route path = "/MARTA" component={Marta} />
                     <Route render={() => (
                         <p>Page not found: No Soup for you!</p>
                         )} />
---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => (
+    <div>
+        <h1>Welcome to React-Router!</h1>
+        <h3>Please select an option from the above menu</h3>
+    </div>
+)
+
+export default Home;
